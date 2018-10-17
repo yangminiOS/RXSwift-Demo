bf3856ef0555f31eceea86ec8a808b3b7f35cb56
yangmin <yangmin@silvrr.com>
2018-10-17 12:04:48 +0800

    提交

--- RXTest/Register/RegisterViewController.swift
+++ RXTest/Register/RegisterViewController.swift
@@ -28,6 +28,7 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("sdfa")
     }
 
     override func didReceiveMemoryWarning() {
